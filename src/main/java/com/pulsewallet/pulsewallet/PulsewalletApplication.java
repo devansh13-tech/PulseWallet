fe80d7527762213d3e28ff9f2ae0a5b8777f4f0a
devansh13-tech <devanshchaudhari13@gmail.com>
2026-08-20 01:38:28 +0530

    Initial PulseWallet setup

---
+++ src/main/java/com/pulsewallet/pulsewallet/PulsewalletApplication.java
@@ -0,0 +1,13 @@
+package com.pulsewallet.pulsewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsewalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsewalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulsewallet/pulsewallet/PulsewalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsewallet.pulsewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsewalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
